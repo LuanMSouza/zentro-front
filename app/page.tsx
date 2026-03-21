a8e4652dd953ee9e1011f25a9e3bc0525492da4a
Luan <luan.soya@gmail.com>
2026-03-21 12:20:04 -0300

    analitcs

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,48 @@
+'use client'
+
 import DashBoard from '@/app/dashboard/pages'
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  // 
+
+  const trackAccess = () => {
+    const params = new URLSearchParams(window.location.search);
+
+    const payload = JSON.stringify({
+      projeto_nome: 'Zentro',
+      pagina_path: window.location.pathname,
+      url_completa: window.location.href,
+      referrer: document.referrer || 'direto',
+      utm_source: params.get('utm_source') || null,
+      utm_medium: params.get('utm_medium') || null,
+      largura_tela: window.innerWidth,
+      idioma: navigator.language,
+      user_agent: navigator.userAgent,
+    });
+
+    const url = "https://api.analitcs.dvls.com.br/api/track";
+
+    if (navigator.sendBeacon) {
+      const blob = new Blob([payload], { type: 'application/json' });
+      navigator.sendBeacon(url, blob);
+    } else {
+      fetch(url, {
+        method: 'POST',
+        body: payload,
+        headers: { 'Content-Type': 'application/json' },
+        keepalive: true // Garante que a requisição termine mesmo se sair da página
+      }).catch(() => { }); // Falha silenciosa
+    }
+  };
+
+  useEffect(() => {
+    trackAccess()
+  }, [])
+
+  // 
+
   return (
     <>
       <DashBoard />
